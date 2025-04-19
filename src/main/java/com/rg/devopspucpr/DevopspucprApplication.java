0c74bd36fd1f49c2fd8ac6b3d3aa7c633e2308f4
Renan <renanfgfg@gmail.com>
2025-04-19 10:51:32 -0300

    first commit

---
+++ src/main/java/com/rg/devopspucpr/DevopspucprApplication.java
@@ -0,0 +1,13 @@
+package com.rg.devopspucpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopspucprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopspucprApplication.class, args);
+	}
+
+}
